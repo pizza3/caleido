bd7ae5c1243c83cb8a0c6fedc9f84639a88f7bc1
pizza3 <yugam.dhuriya@gmail.com>
2019-12-19 03:54:43 +0530

    Updates

---
+++ precache-manifest.37ebf1186da008887c2a80eac6fdd121.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3790360d13d44cbe7974bfdd0f519840",
+    "url": "/caleido/index.html"
+  },
+  {
+    "revision": "7a7187e7f294f191e3ab",
+    "url": "/caleido/static/css/main.23962010.chunk.css"
+  },
+  {
+    "revision": "0d9cb73c7795e5d19a4e",
+    "url": "/caleido/static/js/2.f26d6b6e.chunk.js"
+  },
+  {
+    "revision": "7a7187e7f294f191e3ab",
+    "url": "/caleido/static/js/main.d442321c.chunk.js"
+  },
+  {
+    "revision": "9f2c1cacd4c9c75200d5",
+    "url": "/caleido/static/js/runtime-main.9b857941.js"
+  }
+]);
--- src/styles.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const AppContain = styled.div`
-    position: relative;
-    display: flex;
-    flex-direction: row;
-    height: calc(100vh - 50px);
-`
