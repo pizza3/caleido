fe8254f83f652358d6f466f82e90752d61fcbb64
pizza3 <yugam.dhuriya@gmail.com>
2020-06-05 19:30:12 +0530

    Updates

---
+++ precache-manifest.2cddd72411c6b276b641acc5049d0a35.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffbd1dc3b06a1d3f0705aa09c4e93d32",
+    "url": "/caleido/index.html"
+  },
+  {
+    "revision": "db05e1c2ba60f9a366b7",
+    "url": "/caleido/static/js/2.52d7601f.chunk.js"
+  },
+  {
+    "revision": "56496311ac96f477256c",
+    "url": "/caleido/static/js/main.3123257a.chunk.js"
+  },
+  {
+    "revision": "9f2c1cacd4c9c75200d5",
+    "url": "/caleido/static/js/runtime-main.9b857941.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/caleido/precache-manifest.e94a149ad6806c5e7b16883fb5155f3c.js"
+  "/caleido/precache-manifest.2cddd72411c6b276b641acc5049d0a35.js"
 );
 
 self.addEventListener('message', (event) => {
